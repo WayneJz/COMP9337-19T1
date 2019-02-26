ec62ff1d16e0526ba362ec710bcb4e84e307a48c
WayneJz <waynezjiang@gmail.com>
2019-02-26 19:27:36 +1100

    run modified

--- lab1/run.py
+++ lab1/run.py
@@ -1,6 +1,7 @@
 import time
 import string
 import random
+import matplotlib.pyplot as plot
 
 # from tempaes import *
 # from tempdes import *
@@ -8,21 +9,50 @@ import random
 # from temprsa import *
 # from tempsha1 import *
 
+'''
+    Structure Definition for self.type_size
 
-generator = {'DES': [8, 64, 512, 4096, 32768, 262144, 2047152],
-            'AES': [8, 64, 512, 4096, 32768, 262144, 2047152],
-            'HMACs': [8, 64, 512, 4096, 32768, 262144, 2047152],
-            'SHA-1': [8, 64, 512, 4096, 32768, 262144, 2047152],
-            'RSA': [2, 4, 8, 16, 32, 64, 128]}
+    {Algorithm Name : [[File Size, Encryption Time, Decryption Time]...], ...}
+'''
+
+class Testrun:
+    def __init__(self):
+        self.type_size = {'DES': [[8, 0], [64, 0], [512, 0], [4096, 0], [32768, 0], [262144, 0], [2047152, 0]],
+                        'AES': [[8, 0], [64, 0], [512, 0], [4096, 0], [32768, 0], [262144, 0], [2047152, 0]],
+                        'HMACs': [[8, 0], [64, 0], [512, 0], [4096, 0], [32768, 0], [262144, 0], [2047152, 0]],
+                        'SHA-1': [[8, 0], [64, 0], [512, 0], [4096, 0], [32768, 0], [262144, 0], [2047152, 0]],
+                        'RSA': [[2, 0], [4, 0], [8, 0], [16, 0], [32, 0], [64, 0], [128, 0]]}
+        self.time = None
+
+    def generator(self):
+        for type in self.type_size.keys():
+            for size in self.type_size[type]:
+                self.file_generator(type, size[0])
 
 
-def file_generator(type, size):
-    file_name = type + '_' + str(size) + '.txt'
-    with open(file_name, 'w') as file:
-        for _ in range(0, size):
-            file.write(random.choice(string.printable))
+    def file_generator(self, type, size):
+        file_name = type + '_' + str(size) + '.txt'
+        with open(file_name, 'w') as file:
+            for _ in range(0, size):
+                file.write(random.choice(string.printable))
 
+    def function_call(self):
+        pass
 
+    def timer(self, flag='Stop'):
+        if flag == 'Start':
+            self.time = time.time()
+        else:
+            return time.time() - self.time
+
+    def visualization(self):
+        plot.bar(range(len(self.type_size['DES'])), [x[1] for x in self.type_size['DES']], 
+        tick_label=[y[0] for y in self.type_size['DES']], color='rgb')
+        plot.title('DES')
+        plot.xlabel('File Size')
+        plot.ylabel('Time Consumed')
+        plot.show()
 
 if __name__ == "__main__":
-    file_generator('AES', 256)
+    testrun = Testrun()
+    testrun.visualization()
