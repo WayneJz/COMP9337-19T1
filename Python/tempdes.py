ce2cf2f21dc37b79483ee0cc2d113565c9516ca2
US579 <37062041+US579@users.noreply.github.com>
2019-02-27 15:34:01 +1100

    Update tempdes.py

--- Python/tempdes.py
+++ Python/tempdes.py
@@ -1,28 +1,22 @@
 #encoding=utf-8
 from Crypto.Cipher import DES
-from Crypto import Random
-import sys
 import binascii
 
 def read_file(name):
-    lis_pool = []
-    patch = '0'
-    global  blocksize
+    patch = '\x00'
+    blocksize = 8
     with open(name) as f:
         a =  f.read()
 
     if a:
         for i in range(0, len(a), blocksize):
             if i + blocksize < len(a):
-                lis_pool.append(a[i:i + blocksize])
+                pass
             else:
-                b = a[i:] + patch * (8 - len(a[i:]))
+                patch_num = 8 - len(a[i:])
+                b = a + patch * (patch_num)
 
-                lis_pool.append(b)
-
-
-    return lis_pool
-
+    return b
 
 def DES_CBC_enc(cbc_key,iv,plain_text):
     des1 = DES.new(cbc_key, DES.MODE_CBC, iv)
@@ -36,30 +30,36 @@ def  DES_CBC_dec(cbc_key,iv,cipher_text):
     msg_text = des2.decrypt(cipher_text)
     return msg_text
 
+def run(iv,key,inputfile, outputfile):
+
+    test_file = read_file(inputfile)
+
+    cbc_key = binascii.unhexlify(key)
+    iv = binascii.unhexlify(iv)
+
+    cipher = DES_CBC_enc(cbc_key, iv, test_file)
+    with open(outputfile, mode='a') as file1:
+        file1.write(cipher)
+
+    decipher = DES_CBC_dec(cbc_key, iv, cipher)
+    with open('deciper_file.txt', mode='a') as file:
+        file.write(decipher)
+
 
 
 
 if __name__ == '__main__':
+
     '''''''''
     iv,key,inputfile,outputfile = [sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]]
     
     '''''''''
-    blocksize = 8
-    cbc_key = "\x40\xfe\xdf\x38\x6d\xa1\x3d\x57"
+    key = '40fedf386da13d57'
+    iv = 'fedcba9876543210'
+    run(iv, key, 'test.txt','tes.des')
 
 
-    test_file  = read_file("test2.txt")
-    print(test_file)
 
-
-    for i in test_file:
-        iv = '\xfe\xdc\xba\x98\x76\x54\x32\x10'
-        cipher = DES_CBC_enc(cbc_key,iv,i)
-        print(cipher)
-        decipher = DES_CBC_dec(cbc_key,iv,cipher)
-        with open('dec_file.txt',mode='wa') as file:
-                file.write(decipher)
-        file.close()
 
 
 
